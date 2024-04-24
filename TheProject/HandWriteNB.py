8ac5a3ef631e6d4adb77a0e7daf834b6bd432473
Jonmata <jonna.mata2004@gmail.com>
2024-04-23 21:20:21 -0400

    I am pushing my work into the main repo
    Jonathan Mata work on the research project

---
+++ TheProject/HandWriteNB.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+
+class NaiveBayes:
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self._classes = np.unique(y)
+        n_classes = len(self._classes)
+
+        # calculate mean, var, and prior for each class
+        self._mean = np.zeros((n_classes, n_features), dtype=np.float64)
+        self._var = np.zeros((n_classes, n_features), dtype=np.float64)
+        self._priors = np.zeros(n_classes, dtype=np.float64)
+
+        for idx, c in enumerate(self._classes):
+            X_c = X[y == c]
+            self._mean[idx, :] = X_c.mean(axis=0)
+            self._var[idx, :] = X_c.var(axis=0)
+            self._priors[idx] = X_c.shape[0] / float(n_samples)
+
+    def predict(self, X):
+        y_pred = [self._predict(x) for x in X]
+        return np.array(y_pred)
+
+    def _predict(self, x):
+        posteriors = []
+
+        # calculate posterior probability for each class
+        for idx, c in enumerate(self._classes):
+            prior = np.log(self._priors[idx])
+            posterior = np.sum(np.log(self._pdf(idx, x)))
+            posterior = prior + posterior
+            posteriors.append(posterior)
+
+        # return class with highest posterior probability
+        return self._classes[np.argmax(posteriors)]
+
+    def _pdf(self, class_idx, x):
+        mean = self._mean[class_idx]
+        var = self._var[class_idx]
+        numerator = np.exp(-((x - mean) ** 2) / (2 * var))
+        denominator = np.sqrt(2 * np.pi * var)
+        return numerator / denominator
+
+
+# Testing
+if __name__ == "__main__":
+    # Imports
+    from sklearn.model_selection import train_test_split
+    from sklearn import datasets
+    import pandas as pb 
+
+    def accuracy(y_true, y_pred):
+        accuracy = np.sum(y_true == y_pred) / len(y_true)
+        return accuracy
+
+    data = pb.read_excel("/Users/jonathanmata/Documents/Ai_Project/Combined Data with averages.xlsx")
+
+    x= data.loc[:,('HW Average','PJ01 ','EX 01','Quiz Average')]
+
+    x['HW Average'] *= 5
+
+    grade = data['Grade']
+
+    X_train, X_test,Y_train,Y_test = train_test_split(x.values ,grade,random_state=42,test_size=.30)
+
+    nb = NaiveBayes()
+    nb.fit(X_train, Y_train)
+    predictions = nb.predict(X_test)
+
+    print("Naive Bayes classification accuracy", accuracy(Y_test, predictions))
